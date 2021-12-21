4b99146cbd136c0e03be6cac523c19e411e0720c
andres_otalvaro <aotalvaros@antioquia.gov.co>
2021-12-21 14:38:38 -0500

    Control props alternativo

--- src/02-component-patterns/hooks/useProducts.ts
+++ src/02-component-patterns/hooks/useProducts.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { onChangeArgs, Product } from "../interfaces/interface";
 
 interface useProductArgs {
@@ -10,14 +10,8 @@ interface useProductArgs {
 export const useProducts = ({ onChange, product, value = 0  }: useProductArgs) => {
     
     const [counter, setCounter] = useState(value);
-
-    const isControlled = useRef(!!onChange); //una bandera para saber si el onChange esta siendo controlado
     
     const increaseBy = (value: number) => {
-
-        if (isControlled.current) {
-            return onChange!({ count: value, product});
-        }
 
         const newValue = Math.max(counter + value, 0) 
         setCounter( newValue);
--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -3,29 +3,22 @@ import { Product, ProductInCart } from "../interfaces/interface";
 
 
 export const useShoppingCart = () => {
-   /* El shoppingCart sera un objeto cuya llaves sera un id de los articulos, el cual apunta apunta a un objeto el cual tenemos el 
-        producto y la cantidad que esta llevando */
-    const [shoppingCart, setShoppingCart] = useState<{ [key:string]: ProductInCart }>({}); // El useState maneja un objeto, y dentro tendremos x cantidad de llaves(string) y los valores de ese objeto son de tipo ProductInCart 
+
+    const [shoppingCart, setShoppingCart] = useState<{ [key:string]: ProductInCart }>({}); 
 
     const onProductCountChange = ({ count, product }: {count: number, product: Product}) => {
-        // console.log( count, product);
         
         setShoppingCart( oldShoppingCart => {
-            
-            const productInCart: ProductInCart = oldShoppingCart[product.id] || { ...product, count: 0 };
 
-            if (Math.max(productInCart.count + count, 0) > 0) {
-                productInCart.count += count   
-
-                return {
-                    ...oldShoppingCart,
-                    [product.id]: productInCart
-                }
+            if (count === 0) {
+                const {[product.id]: toDelete, ...rest} = oldShoppingCart;
+                return rest;
             }
 
-            //Borra el producto
-            const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-            return rest
+            return {
+                ...oldShoppingCart,
+                [product.id]:{...product, count}
+            }
 
         });
     };
